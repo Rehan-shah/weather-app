25d69d056119cab9e7646a98267cc5aba89cb37c
Rehan-shah <Rehansnehalshah1@gmail.com>
2023-11-16 13:17:07 +0530

    Initial Commit

---
+++ weather app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weather app
+//
+//  Created by Rehan Shah on 16/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
